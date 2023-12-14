314a8921c7053f465a07e6c6d0ddc83cfd15ad10
AISimonetta <info@simonettaalejandra.com>
2023-12-14 06:17:21 +0000

    search bt name inputfinished and working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { useState, FormEvent } from 'react';
 import './App.css';
 import team from './data/team';
+import FilterName from './components/FilterName/FilterName';
 import EmployeeCards from './components/EmployeeCards/EmployeeCards';
 
 function App() {
+  const [searchName, setSearchName] = useState('');
+
+  const handleSearchByNameInput = (event: FormEvent<HTMLInputElement>) => {
+    const nameInput = event.currentTarget.value.toLowerCase();
+    setSearchName(nameInput);
+  };
+
+  const filteredEmployeed = team.filter((employee) =>
+  employee.name.toLowerCase().includes(searchName)
+);
+
   return (
     <>
       <h1 className='main-title'> | TICKET TRACKER |</h1>
+      <FilterName searchName={searchName} onChange={handleSearchByNameInput} />
       <div className='cards'>
-        {team.map((employee) => (
-          <EmployeeCards key={employee.id} name={employee.name} role={employee.role} />
+        {filteredEmployeed.map((employee) => (
+          <EmployeeCards
+              key={employee.id}
+              name={employee.name}
+              role={employee.role} 
+          />
         ))}
       </div>
     </>
